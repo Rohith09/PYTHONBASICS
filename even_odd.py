7ec36b0f04edf03def4db6f39411b01cd931af21
Rohith09 <kandirohith09@gmail.com>
2017-12-01 13:33:36 +0530

    Finding out whether the  entered number is even or odd.

--- even_odd.py
+++ even_odd.py
@@ -1,8 +1,8 @@
 # program to check whether the entered number is even or odd.
 
-x = int(input("Enter any number:"))
+y = int(input("Enter any number:"))
 
-if x % 2 == 0:
+if y % 2 == 0:
     print("The entered number is even")
 else:
     print("The entered number is odd")
