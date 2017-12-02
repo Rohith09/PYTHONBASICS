305eac574def1deb79d3c00f49c15c474d8e8ddc
Rohith09 <kandirohith09@gmail.com>
2017-12-02 18:28:02 +0530

    Basic Slicing operation on String.

---
+++ string_slicing.py
@@ -0,0 +1,7 @@
+str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print("The length of the above string is ", len(str1))
+print(str1[:len(str1)])
+print(str1[4:20])   # the first value inside the (square bracket) [ ] is inclusive and the value after the (Colon) : is exclusive.
+
+
+
